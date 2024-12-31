59d1bf730f185396b7a60f6f4950fee2eba39c3b
muhammad akasha <akashaahmed95@gmail.com>
2024-12-31 23:24:05 +0500

    fix api

--- src/components/UserAd.jsx
+++ src/components/UserAd.jsx
@@ -18,7 +18,7 @@ const UserAd = () => {
     setError(null);
     try {
       const res = await axios.post(
-        `https://geographical-britte-akasha-08aa17f3.koyeb.app//v1/getads`,
+        `https://geographical-britte-akasha-08aa17f3.koyeb.app/api/v1/getads`,
         {},
         { withCredentials: true }
       );
